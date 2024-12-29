b2d65d142e0797d9007cd6be97b6f243829ca5f8
MasnunMuhaemin <108683368+MasnunMuhaemin@users.noreply.github.com>
2024-12-29 21:06:15 +0700

    TRL-38: Fix Responsive Our Service

--- resources/views/components/fasilitashotel.blade.php
+++ resources/views/components/fasilitashotel.blade.php
@@ -119,15 +119,9 @@
     display: none !important;
   }
 
-  @media (min-width: 1025px) {
+  @media (min-width: 1024px) {
     .hidden-tablet {
       display: block !important;
-    }
-  }
-
-  @media (min-width: 768px) and (max-width: 1024px) {
-    .hidden-tablet {
-      display: none !important; 
     }
   }
 </style>
