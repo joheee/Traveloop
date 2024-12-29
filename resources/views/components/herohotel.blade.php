e548780dc4897975570f2f89d2ed6c12257ccbb2
MasnunMuhaemin <108683368+MasnunMuhaemin@users.noreply.github.com>
2024-12-30 00:54:55 +0700

    TRL-1: Slicing UI Landing Page (on proggress)

--- resources/views/components/herohotel.blade.php
+++ resources/views/components/herohotel.blade.php
@@ -1,11 +1,11 @@
 <div class="py-5">
     <div class="row align-items-center">
         <div class="col-md-6 text-start text-md-start">
-            <h2 class="judul fw-bold">Dapatkan pengalaman yang <br> tak terlupakan dengan satu <br> kali klik</h2>
-            <p class="mt-3">
+            <h3 class="judul lg-fs-1 fw-bold">Dapatkan pengalaman yang <br> tak terlupakan dengan satu <br> kali klik</h3>
+            <p class="mt-3 fs-6 fs-sm-5 fs-md-4">
                 Kami menyediakan hotel berkualitas di berbagai lokasi dengan pemesanan yang mudah dan praktis untuk kenyamanan perjalanan Anda.
             </p>
-            <a href="#" class="btn btn-lg text-white rounded mt-5 d-flex justify-content-start" style="background-color: #0ea5e9; width: 175px">Pesan Sekarang</a>
+            <a href="#" class="btn btn-md text-white rounded mt-5 d-flex justify-content-start" style="background-color: #0ea5e9; width: 140px">Pesan Sekarang</a>
         </div>
         <div class="col-md-6 d-none d-md-flex justify-content-center">
             <div class="stacked-images">
@@ -19,28 +19,3 @@
         </div>
     </div>
   </div>
-  
-  <style>
-    @media (max-width: 576px) { 
-        .stacked-images {
-            display: none;
-        }
-    }
-  
-    @media (min-width: 577px) and (max-width: 768px) {
-        .stacked-images img {
-            width: 160px;
-            height: 240px;
-            margin-left: 0; 
-        }
-        .stacked-images img:last-child {
-            margin-top: -15px;
-        }
-    }
-  
-    @media (min-width: 769px) {
-        .stacked-images img:last-child {
-            margin-top: -20px; 
-        }
-    }
-  </style>  
--- resources/views/components/navigation.blade.php
+++ resources/views/components/navigation.blade.php
@@ -1,7 +1,15 @@
 <nav class="navbar navbar-expand-lg sticky-top" style="background-color: #0ea5e9">
-    <div class="container-fluid">
+    <div class="container">
         <a class="navbar-brand text-white" href="#">Traveloop</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" id="navbar-toggler">
+        <button 
+            class="navbar-toggler p-1 w-auto h-auto fs-6 d-md-none" 
+            type="button" 
+            data-bs-toggle="collapse" 
+            data-bs-target="#navbarSupportedContent" 
+            aria-controls="navbarSupportedContent" 
+            aria-expanded="false" 
+            aria-label="Toggle navigation" 
+            id="navbar-toggler">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
